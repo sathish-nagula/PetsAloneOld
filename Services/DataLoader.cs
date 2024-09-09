eac60e00ed894c7a94a230b4d807e5263a436ea6
Sathish <sathish.nagula@outlook.com>
2024-09-09 03:25:18 +0100

    Implemented DataLoader class to load 100 missing pets fake data

--- Services/DataLoader.cs
+++ Services/DataLoader.cs
@@ -1,13 +1,35 @@
-﻿using PetsAlone.Models;
+﻿using Bogus;
+using PetsAlone.Models;
+using PetsAlone.Models.Enums;
 using PetsAlone.ServiceContracts;
 
 namespace PetsAlone.Services
 {
     public class DataLoader : IDataLoader
     {
+        private static readonly List<Pet> _pets;
+
+        static DataLoader()
+        {
+            _pets = GeneratePets();
+        }
+
         public Task<List<Pet>> LoadPetsAsync()
         {
-            throw new NotImplementedException();
+            return Task.FromResult(_pets);
+        }
+
+        private static List<Pet> GeneratePets()
+        {
+            var faker = new Faker<Pet>()
+                .RuleFor(p => p.Id, f => f.IndexFaker + 1)
+                .RuleFor(p => p.Name, f => f.Name.FirstName())
+                .RuleFor(p => p.PetType, f => f.PickRandom<PetType>())
+                .RuleFor(p => p.Description, f => f.Lorem.Sentence())
+                .RuleFor(p => p.IsMissing, f => true)
+                .RuleFor(p => p.DateMissing, f => f.Date.Past(2));
+
+            return faker.Generate(100);
         }
     }
 }
