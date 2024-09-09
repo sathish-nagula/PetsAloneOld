b9524a25c84b540e0116f518f21a8d46a9eaa199
Sathish <sathish.nagula@outlook.com>
2024-09-09 03:10:02 +0100

    Added service classes that implements the service contracts

---
+++ Services/DataLoader.cs
@@ -0,0 +1,13 @@
+﻿using PetsAlone.Models;
+using PetsAlone.ServiceContracts;
+
+namespace PetsAlone.Services
+{
+    public class DataLoader : IDataLoader
+    {
+        public Task<List<Pet>> LoadPetsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/PetsService.cs
@@ -0,0 +1,18 @@
+﻿using PetsAlone.Models;
+using PetsAlone.ServiceContracts;
+
+namespace PetsAlone.Services
+{
+    public class PetsService : IPetService
+    {
+        public Task AddPetAsync(Pet pet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Pet>> GetPetsAsync(string typeFilter, string sortOrder, int page = 1, int pageSize = 10)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
